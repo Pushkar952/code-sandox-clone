763b07c41dca72d1b4f2896ea67b88b0bc53246e
Pushkar952 <kumar.pus95@gmail.com>
2022-08-31 15:49:54 +0530

    cells reducer complet setup

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -18,7 +18,7 @@ export interface DeleteCellAction {
 export interface InsertCellBeforeAction {
     type: ActionType.INSERT_CELL_BEFORE;
     payload: {
-        id: string;
+        id: string | null;
         type: CellTypes;
     }
 }
--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,7 +1,8 @@
 
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
-import {Cell} from "../cell";
+import { Cell } from "../cell";
+import produce from "immer";
 
 interface CellsState{
     loading: boolean;
@@ -18,31 +19,52 @@ const initialState: CellsState = {
     data: {}
 };
 
-const reducer = (state: CellsState = initialState, action: Action): CellsState => {
-    switch (action.type) { 
+const reducer = produce((state: CellsState = initialState, action: Action): CellsState => {
+    switch (action.type) {
         case ActionType.UPDATE_CELL:
             const { id, content } = action.payload;
-            return {
-                ...state,
-                data: {
-                    ...state.data,
-                    [id]: {
-                        ...state.data[id],
-                        content: content
-                    }
-                }
-        
-            };
+            state.data[id].content = content;
+            return state;
         case ActionType.DELETE_CELL:
+            delete state.data[action.payload];
+            state.order = state.order.filter(id => id !== action.payload);
             return state;
         case ActionType.MOVE_CELL:
+            const { direction } = action.payload;
+            const index = state.order.findIndex((id) => id === action.payload.id);
+            const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+            if (targetIndex < 0 || targetIndex >= state.order.length) {
+                return state;
+            }
+            state.order[index] = state.order[targetIndex];
+            state.order[targetIndex] = action.payload.id;
             return state;
         case ActionType.INSERT_CELL_BEFORE:
+            const cell: Cell = {
+                id: randomId(),
+                type: action.payload.type,
+                content: ''
+            };
+            state.data[cell.id] = cell;
+            const foundIndex = state.order.findIndex((id) => id === action.payload.id);
+            if (foundIndex < 0) {
+                state.order.push(cell.id);
+            }   
+            else
+            {
+                state.order.splice(foundIndex, 0, cell.id);
+            }
+                
             return state;
         default:
             return state;
     }
    
-};
+});
+
+const randomId = (): string => { 
+    return Math.random().toString(36).substring(2, 5);;
+}
 
 export default reducer;
